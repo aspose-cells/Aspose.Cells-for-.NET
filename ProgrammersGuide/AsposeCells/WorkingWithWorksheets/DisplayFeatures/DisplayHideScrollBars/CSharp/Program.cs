602c5edf72d8efc8b7ff4c3f6c2713f7084dc073
iqbal-aspose <muhammad.iqbal@aspose.com>
2013-03-13 16:41:07 +0500

    Examples addition Set 2

---
+++ ProgrammersGuide/AsposeCells/WorkingWithWorksheets/DisplayFeatures/DisplayHideScrollBars/CSharp/Program.cs
@@ -0,0 +1,41 @@
+//////////////////////////////////////////////////////////////////////////
+// Copyright 2001-2013 Aspose Pty Ltd. All Rights Reserved.
+//
+// This file is part of Aspose.Cells. The source code in this file
+// is only intended as a supplement to the documentation, and is provided
+// "as is", without warranty of any kind, either expressed or implied.
+//////////////////////////////////////////////////////////////////////////
+using System.IO;
+
+using Aspose.Cells;
+
+namespace DisplayHideScrollBars
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // The path to the documents directory.
+            string dataDir = Path.GetFullPath("../../../Data/");
+
+            //Creating a file stream containing the Excel file to be opened
+            FileStream fstream = new FileStream(dataDir + "book1.xls", FileMode.Open);
+
+            //Instantiating a Workbook object
+            //Opening the Excel file through the file stream
+            Workbook workbook = new Workbook(fstream);
+
+            //Hiding the vertical scroll bar of the Excel file
+            workbook.Settings.IsVScrollBarVisible = false;
+
+            //Hiding the horizontal scroll bar of the Excel file
+            workbook.Settings.IsHScrollBarVisible = false;
+
+            //Saving the modified Excel file
+            workbook.Save(dataDir + "output.xls");
+
+            //Closing the file stream to free all resources
+            fstream.Close();
+        }
+    }
+}
