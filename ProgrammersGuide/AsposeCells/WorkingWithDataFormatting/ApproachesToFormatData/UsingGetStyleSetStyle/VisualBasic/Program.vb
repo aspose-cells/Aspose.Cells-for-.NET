5504a4a84d7274e21e4a15778068b9aa34038a80
Fatima Ahmad <fatima.rabbiya@aspose.com>
2013-10-22 11:44:09 +0500

    New Examples Added

---
+++ ProgrammersGuide/AsposeCells/WorkingWithDataFormatting/ApproachesToFormatData/UsingGetStyleSetStyle/VisualBasic/Program.vb
@@ -0,0 +1,71 @@
+'////////////////////////////////////////////////////////////////////////
+' Copyright 2001-2013 Aspose Pty Ltd. All Rights Reserved.
+'
+' This file is part of Aspose.Cells. The source code in this file
+' is only intended as a supplement to the documentation, and is provided
+' "as is", without warranty of any kind, either expressed or implied.
+'////////////////////////////////////////////////////////////////////////
+
+Imports Microsoft.VisualBasic
+Imports System.IO
+
+Imports Aspose.Cells
+Imports System.Drawing
+
+Namespace UsingGetStyleSetStyle
+	Public Class Program
+		Public Shared Sub Main(ByVal args() As String)
+			' The path to the documents directory.
+			Dim dataDir As String = Path.GetFullPath("../../../Data/")
+
+			' Create directory if it is not already present.
+			Dim IsExists As Boolean = System.IO.Directory.Exists(dataDir)
+			If (Not IsExists) Then
+				System.IO.Directory.CreateDirectory(dataDir)
+			End If
+
+			'Instantiating a Workbook object
+			Dim workbook As New Workbook()
+
+			'Obtaining the reference of the first worksheet
+			Dim worksheet As Worksheet = workbook.Worksheets(0)
+
+			'Accessing the "A1" cell from the worksheet
+			Dim cell As Cell = worksheet.Cells("A1")
+
+			'Adding some value to the "A1" cell
+			cell.PutValue("Hello Aspose!")
+
+			'Defining a Style object
+			Dim style As Aspose.Cells.Style
+
+			'Get the style from A1 cell
+			style = cell.GetStyle()
+
+			'Setting the vertical alignment
+			style.VerticalAlignment = TextAlignmentType.Center
+
+			'Setting the horizontal alignment
+			style.HorizontalAlignment = TextAlignmentType.Center
+
+			'Setting the font color of the text
+			style.Font.Color = Color.Green
+
+			'Setting to shrink according to the text contained in it
+			style.ShrinkToFit = True
+
+			'Setting the bottom border color to red
+			style.Borders(BorderType.BottomBorder).Color = Color.Red
+
+			'Setting the bottom border type to medium
+			style.Borders(BorderType.BottomBorder).LineStyle = CellBorderType.Medium
+
+			'Applying the style to A1 cell
+			cell.SetStyle(style)
+
+			'Saving the Excel file
+			workbook.Save(dataDir & "book1.xls")
+
+		End Sub
+	End Class
+End Namespace
