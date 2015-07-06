b36f1bde87b66ef537b1624db3f8d5f40995b193
asposemarketplace <marketplace@aspose.com>
2015-07-06 13:55:59 +0500

    New Examples added in Aspose.Cells Vs OpenXML Spreadsheets in v 1.1

---
+++ Plugins/Aspose.Cells Vs OpenXML Spreadsheets/Code Comparison of Common Features/Open spreadsheet document for read-only access/Aspose.Cells/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspose.Cells
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FileName = @"E:\Aspose\Aspose Vs OpenXML\Aspose.Cells Vs OpenXML Spreadsheet v 1.1\SampleFiles\Open a spreadsheet document for read-only access.xlsx";
+            OpenSpreadsheetDocumentReadonly(FileName);
+        }
+        public static void OpenSpreadsheetDocumentReadonly(string filepath)
+        {
+            // Open a SpreadsheetDocument based on a filepath.
+            Workbook workbook = new Workbook(filepath);
+        }
+    }
+}
---
+++ Plugins/README.md
@@ -0,0 +1,4 @@
+## Aspose.Cells for .NET Plugins
+
+This section contains the following Platforms
+* [Aspose.Cells Vs OpenXML Spreadsheets](Aspose.Cells Vs OpenXML Spreadsheets)
