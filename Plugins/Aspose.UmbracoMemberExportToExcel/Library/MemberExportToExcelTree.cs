36ec1ea722a2bb05d1c074af3e3122e97d7eb601
zaheertariq <zaheer.tariq@aspose.com>
2015-07-16 16:13:49 +0500

    Initial commit for Export Members to Excel

---
+++ Plugins/Aspose.UmbracoMemberExportToExcel/Library/MemberExportToExcelTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using umbraco.cms.presentation.Trees;
+using umbraco.BusinessLogic.Actions;
+
+namespace Aspose.UmbracoMemberExportToExcel.Library
+{
+    public class MemberExportToExcelTree : BaseTree
+    {
+        public MemberExportToExcelTree(string application) : base(application) { }
+
+        protected override void CreateRootNode(ref XmlTreeNode rootNode)
+        {
+            rootNode.NodeID = System.Guid.NewGuid().ToString();
+            rootNode.Action = "javascript:openExportToExcelPage()";
+            rootNode.Menu.Clear();
+            rootNode.Menu.Add(ActionRefresh.Instance);
+            rootNode.Icon = "../../plugins/AsposeMemberExport/Images/aspose.ico";
+            rootNode.OpenIcon = "../../plugins/AsposeMemberExport/Images/aspose.ico";
+        }
+
+        public override void Render(ref XmlTree tree)
+        {
+        }
+
+        public override void RenderJS(ref System.Text.StringBuilder Javascript)
+        {
+            Javascript.Append(
+               @"function openExportToExcelPage() {
+                 UmbClientMgr.contentFrame('/umbraco/plugins/AsposeMemberExport/ExportToExcel.aspx');
+                }");
+        }
+    }
+}
