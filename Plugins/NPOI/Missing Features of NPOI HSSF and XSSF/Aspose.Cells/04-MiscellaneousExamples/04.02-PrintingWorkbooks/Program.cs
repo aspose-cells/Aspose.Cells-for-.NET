817cd4c7727b726aecd9c8744e244722912fe908
zaheertariq <zaheer.tariq@aspose.com>
2015-09-03 16:15:00 +0500

    Initial commit - Missing Features of NPOI HSSF and XSSF

---
+++ Plugins/NPOI/Missing Features of NPOI HSSF and XSSF/Aspose.Cells/04-MiscellaneousExamples/04.02-PrintingWorkbooks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text; using Aspose.Cells;
+using Aspose.Cells.Rendering;
+
+namespace _04._02_PrintingWorkbooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiating a Workbook object
+            Workbook workbook = new Workbook("../../data/test.xlsx");
+
+            //Create an object for ImageOptions
+            ImageOrPrintOptions imgOptions = new ImageOrPrintOptions();
+
+            //Get the first worksheet
+            Worksheet sheet = workbook.Worksheets[0];
+
+            //Create a SheetRender object with respect to your desired sheet
+            SheetRender sr = new SheetRender(sheet, imgOptions);
+
+            //Print the worksheet
+            sr.ToPrinter("Samsung ML-1520 Series");
+        }
+    }
+}
---
+++ Plugins/NPOI/Missing Features of NPOI HSSF and XSSF/Aspose.Cells/04-MiscellaneousExamples/04.03-ConvertSpreadsheetToPDF/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text; using Aspose.Cells;
+
+namespace _04._03_ConvertSpreadsheetToPDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Workbook workbook = new Workbook("../../data/test.xlsx");
+
+            //Save the document in PDF format
+            workbook.Save("AsposeConvert.pdf", SaveFormat.Pdf);
+        }
+    }
+}
