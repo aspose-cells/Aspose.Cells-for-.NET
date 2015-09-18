b035e95fa8b3d128a68f5315d994dad13b7cf907
zaheertariq <zaheer.tariq@aspose.com>
2015-09-18 10:59:24 +0500

    Aspose.Excel.GridViewExport initial commit

---
+++ Plugins/VisualStudio/Aspose.Excel.GridViewExport/Aspose.Excel.GridViewExport.Website/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace Aspose.Excel.GridViewExport.Website
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                BindGrid();
+            }
+        }
+
+        private void BindGrid()
+        {
+            DataTable dataTable = new DataTable("Products");
+            dataTable.Columns.Add("Product ID", typeof(Int32));
+            dataTable.Columns.Add("Product Name", typeof(string));
+            dataTable.Columns.Add("Units In Stock", typeof(Int32));
+
+
+            for (int index = 1; index < 50; index++)
+            {
+                DataRow dr = dataTable.NewRow();
+                dr[0] = index;
+                dr[1] = "Product - " + dr[0];
+                dr[2] = index + 5;
+                dataTable.Rows.Add(dr);
+            }
+
+            ExportGridViewToExcel1.ExportDataSource = dataTable;
+            ExportGridViewToExcel1.DataSource = dataTable;
+            ExportGridViewToExcel1.DataBind();
+        }
+
+        protected void ExportGridViewToExcel1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            ExportGridViewToExcel1.PageIndex = e.NewPageIndex;
+            BindGrid();
+        }
+    }
+}
---
+++ Plugins/VisualStudio/README.md
@@ -0,0 +1,5 @@
+# Aspose.Cells for Visual Studio
+
+We have developed the following examples for Visual Studio using Aspose.Cells
+
+* [Export GridView To Excel](Aspose.Excel.GridViewExport)
