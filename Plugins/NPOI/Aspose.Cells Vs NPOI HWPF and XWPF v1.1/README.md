8387fe8a6c71182f892810ba2c542e6bfb0a899e
Assad Mahmood <assadvirgo@gmail.com>
2015-11-05 09:30:57 +0500

    Aspose.Cells Vs NPOI HWPF and XWPF v1.1
    Comparison Examples

---
+++ Plugins/NPOI/Aspose.Cells Vs NPOI HWPF and XWPF v1.1/README.md
@@ -0,0 +1,7 @@
+Project Aspose.Cells for NPOI (HSSF and XSSF) shows how different tasks can be performed using Aspose.Cells .NET APIs in comparison with NPOI. This project is helpful for developers who want to compare NPOI with Aspose.Cells OR migrate from NPOI to Aspose.Cells.
+
+This release of the project have the following comparison examples
+
+* Working With Borders
+* Working With Colors
+* Working With Fonts
---
+++ Plugins/NPOI/Aspose.Cells Vs NPOI HWPF and XWPF v1.1/Working With Colors/Aspose.Cells/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Aspose.Cells
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Workbook workbook = new Workbook(); // Creating a Workbook object
+            workbook.Worksheets.Add();
+            Worksheet worksheet = workbook.Worksheets[0];
+
+            //Accessing cell from the worksheet
+            Cell cell = worksheet.Cells["B2"];
+            Style style = cell.GetStyle();            
+
+            //Setting the foreground color to yellow            
+            style.BackgroundColor = Color.Yellow;
+
+            //Setting the background pattern to vertical stripe
+            style.Pattern = BackgroundType.VerticalStripe;            
+
+            //Saving the modified style to the "B2" cell.
+            cell.SetStyle(style);
+
+            // === Setting Foreground ===
+
+            //Adding custom color to the palette at 55th index
+            Color color = Color.FromArgb(212, 213, 0);
+            workbook.ChangePalette(color, 55);
+
+            //Accessing cell from the worksheet
+            cell = worksheet.Cells["B3"];
+
+            //Adding some value to the cell
+            cell.PutValue("Hello Aspose!");
+
+            workbook.Save("test.xlsx", SaveFormat.Xlsx); //Workbooks can be saved in many formats
+        }
+    }
+}
