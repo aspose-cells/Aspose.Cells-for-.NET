d4df13ec39fc5ddaadbf8a214e73d6e72d4a72ae
Saqib <saqib.masood@aspose.com>
2016-03-10 17:23:23 +0500

    Update ExtractingOLEObjects.cs

--- Examples/CSharp/DrawingObjects/OLE/ExtractingOLEObjects.cs
+++ Examples/CSharp/DrawingObjects/OLE/ExtractingOLEObjects.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.DrawingObjects.OLE
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -66,6 +67,7 @@ namespace Aspose.Cells.Examples.DrawingObjects.OLE
                     FileStream fs = File.Create(fileName);
                     fs.Write(ole.ObjectData, 0, ole.ObjectData.Length);
                     fs.Close();
+                    //ExEnd:1
                 }
 
             }
