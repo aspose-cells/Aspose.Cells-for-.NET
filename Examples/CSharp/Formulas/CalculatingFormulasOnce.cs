32daea67f5c95addef13024060eb0999c745c234
Saqib <saqib.masood@aspose.com>
2016-03-10 17:11:37 +0500

    Update CalculatingFormulasOnce.cs

--- Examples/CSharp/Formulas/CalculatingFormulasOnce.cs
+++ Examples/CSharp/Formulas/CalculatingFormulasOnce.cs
@@ -9,6 +9,7 @@ namespace Aspose.Cells.Examples.Formulas
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -28,6 +29,7 @@ namespace Aspose.Cells.Examples.Formulas
 
             //Print the time after formula calculation
             Console.WriteLine(DateTime.Now);
+            //ExEnd:1
 
         }
     }
