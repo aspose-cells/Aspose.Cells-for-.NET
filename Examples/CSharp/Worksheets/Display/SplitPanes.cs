07d3afef35f6ec477d8ab5868012e745ba14db4f
Saqib <saqib.masood@aspose.com>
2016-03-10 16:35:17 +0500

    Update SplitPanes.cs

--- Examples/CSharp/Worksheets/Display/SplitPanes.cs
+++ Examples/CSharp/Worksheets/Display/SplitPanes.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Worksheets.Display
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -22,6 +23,7 @@ namespace Aspose.Cells.Examples.Worksheets.Display
 
             //Save the excel file
             book.Save(dataDir + "Splitted_out1.out.xls");
+            //ExEnd:1
         }
     }
 }
