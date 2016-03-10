e3b35b9050cb6f70fdafbd9071bead0b68d2118d
Saqib <saqib.masood@aspose.com>
2016-03-10 16:58:30 +0500

    Update ConvertTableToRange.cs

--- Examples/CSharp/Tables/ConvertTableToRange.cs
+++ Examples/CSharp/Tables/ConvertTableToRange.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Tables
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -19,6 +20,7 @@ namespace Aspose.Cells.Examples.Tables
 
             //Save the file
             wb.Save(dataDir + "output.out.xlsx");
+            //ExEnd:1
 
         }
     }
