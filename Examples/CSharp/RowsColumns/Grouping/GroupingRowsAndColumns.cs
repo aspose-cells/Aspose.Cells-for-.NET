6e3379ae69a3bcc7466b1be5f9b8d2ad48d7d092
Saqib <saqib.masood@aspose.com>
2016-03-10 16:26:06 +0500

    Update GroupingRowsAndColumns.cs

--- Examples/CSharp/RowsColumns/Grouping/GroupingRowsAndColumns.cs
+++ Examples/CSharp/RowsColumns/Grouping/GroupingRowsAndColumns.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.RowsColumns.Grouping
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -32,6 +33,7 @@ namespace Aspose.Cells.Examples.RowsColumns.Grouping
 
             //Closing the file stream to free all resources
             fstream.Close();
+            //ExEnd:1
 
             
         }
