821fdd9c1072a8fdb63130fc771aeebccccd9bf6
Saqib <saqib.masood@aspose.com>
2016-03-10 16:32:04 +0500

    Update DisplayHideTabs.cs

--- Examples/CSharp/Worksheets/Display/DisplayHideTabs.cs
+++ Examples/CSharp/Worksheets/Display/DisplayHideTabs.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Worksheets.Display
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -20,6 +21,7 @@ namespace Aspose.Cells.Examples.Worksheets.Display
 
             //Saving the modified Excel file
             workbook.Save(dataDir + "output.out.xls");
+            //ExEnd:1
         }
     }
 }
