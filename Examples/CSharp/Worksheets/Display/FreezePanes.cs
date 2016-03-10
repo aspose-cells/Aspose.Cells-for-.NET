0f3c0669f510eb083bec4ef3ada912a1a6d752f9
Saqib <saqib.masood@aspose.com>
2016-03-10 16:32:31 +0500

    Update FreezePanes.cs

--- Examples/CSharp/Worksheets/Display/FreezePanes.cs
+++ Examples/CSharp/Worksheets/Display/FreezePanes.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Worksheets.Display
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -29,7 +30,7 @@ namespace Aspose.Cells.Examples.Worksheets.Display
 
             //Closing the file stream to free all resources
             fstream.Close();
- 
+            //ExEnd:1
         }
     }
 }
