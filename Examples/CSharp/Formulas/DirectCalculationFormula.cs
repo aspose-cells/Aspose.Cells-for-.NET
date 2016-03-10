5ebe14b76513c68172e819ef5697a94ec86d6313
Saqib <saqib.masood@aspose.com>
2016-03-10 17:12:23 +0500

    Update DirectCalculationFormula.cs

--- Examples/CSharp/Formulas/DirectCalculationFormula.cs
+++ Examples/CSharp/Formulas/DirectCalculationFormula.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Formulas
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -37,6 +38,7 @@ namespace Aspose.Cells.Examples.Formulas
             System.Console.WriteLine("Value of A1: " + cellA1.StringValue);
             System.Console.WriteLine("Value of A2: " + cellA2.StringValue);
             System.Console.WriteLine("Result of Sum(A1:A2): " + results.ToString());
+            //ExEnd:1
 
         }
     }
