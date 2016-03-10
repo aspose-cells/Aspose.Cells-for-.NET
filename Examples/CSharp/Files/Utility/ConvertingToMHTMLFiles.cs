c9c7a7f444a8fd29990da2d4a6ce9cd659b37c82
Saqib <saqib.masood@aspose.com>
2016-03-10 17:17:53 +0500

    Update ConvertingToMHTMLFiles.cs

--- Examples/CSharp/Files/Utility/ConvertingToMHTMLFiles.cs
+++ Examples/CSharp/Files/Utility/ConvertingToMHTMLFiles.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Files.Utility
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -22,6 +23,7 @@ namespace Aspose.Cells.Examples.Files.Utility
 
             //Save the MHT file
             wb.Save(filePath + ".out.mht", sv);
+            //ExEnd:1
         }
     }
 }
