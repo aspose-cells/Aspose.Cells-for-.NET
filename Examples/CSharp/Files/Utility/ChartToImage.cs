ccdfee4dc701374922e7b4a4e16f3fc017f84b73
Saqib <saqib.masood@aspose.com>
2016-03-10 17:16:21 +0500

    Update ChartToImage.cs

--- Examples/CSharp/Files/Utility/ChartToImage.cs
+++ Examples/CSharp/Files/Utility/ChartToImage.cs
@@ -9,6 +9,7 @@ namespace Aspose.Cells.Examples.Files.Utility
     {
         public static void Main(string[] args)
         {
+        	//ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -54,6 +55,7 @@ namespace Aspose.Cells.Examples.Files.Utility
 
             // Display result, so that user knows the processing has finished.
             System.Console.WriteLine("Image generated successfully.");
+            //ExEnd:1
         }
     }
 }
