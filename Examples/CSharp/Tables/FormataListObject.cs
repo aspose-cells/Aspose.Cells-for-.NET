092624a9ab8e14d4d0dffb173ebda32b270ade60
Saqib <saqib.masood@aspose.com>
2016-03-10 17:09:52 +0500

    Update FormataListObject.cs

--- Examples/CSharp/Tables/FormataListObject.cs
+++ Examples/CSharp/Tables/FormataListObject.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Tables
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -231,6 +232,7 @@ namespace Aspose.Cells.Examples.Tables
 
             //Saving the Excel file
             workbook.Save(dataDir + "List_Object.out.xlsx");
+            //ExEnd:1
 
         }
     }
