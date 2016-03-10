e481bfe0f98a4ccadbfa10eecad7696077a78303
Saqib <saqib.masood@aspose.com>
2016-03-10 17:19:27 +0500

    Update EncryptingFiles.cs

--- Examples/CSharp/Files/Utility/EncryptingFiles.cs
+++ Examples/CSharp/Files/Utility/EncryptingFiles.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Files.Utility
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -26,6 +27,7 @@ namespace Aspose.Cells.Examples.Files.Utility
 
             //Save the excel file.
             workbook.Save(dataDir + "encryptedBook1.out.xls");
+            //ExEnd:1
         }
     }
 }
