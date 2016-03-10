1f0537422af99bf356e27c11b492897010e757a6
Saqib <saqib.masood@aspose.com>
2016-03-10 16:36:43 +0500

    Update AddWorksheetsToExistingExcelFile.cs

--- Examples/CSharp/Worksheets/Management/AddWorksheetsToExistingExcelFile.cs
+++ Examples/CSharp/Worksheets/Management/AddWorksheetsToExistingExcelFile.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Worksheets.Management
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -32,6 +33,7 @@ namespace Aspose.Cells.Examples.Worksheets.Management
 
             //Closing the file stream to free all resources
             fstream.Close();
+            //ExEnd:1
             
             
         }
