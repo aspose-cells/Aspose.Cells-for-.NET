692dc18cebc27b12572544de1259e6d36428b204
Saqib <saqib.masood@aspose.com>
2016-03-10 17:25:06 +0500

    Update PictureCellReference.cs

--- Examples/CSharp/DrawingObjects/Pictures/PictureCellReference.cs
+++ Examples/CSharp/DrawingObjects/Pictures/PictureCellReference.cs
@@ -9,6 +9,7 @@ namespace Aspose.Cells.Examples.DrawingObjects.Pictures
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -37,6 +38,7 @@ namespace Aspose.Cells.Examples.DrawingObjects.Pictures
 
             //Save the Excel file.
             workbook.Save(dataDir + "output.out.xls");
+            //ExEnd:1
 
         }
     }
