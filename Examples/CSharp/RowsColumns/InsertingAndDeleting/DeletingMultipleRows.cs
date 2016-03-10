8f5f862ef41d04de44830a4e7666d0acfb882b1f
Saqib <saqib.masood@aspose.com>
2016-03-10 16:06:37 +0500

    Update DeletingMultipleRows.cs

--- Examples/CSharp/RowsColumns/InsertingAndDeleting/DeletingMultipleRows.cs
+++ Examples/CSharp/RowsColumns/InsertingAndDeleting/DeletingMultipleRows.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.RowsColumns.InsertingAndDeleting
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -29,6 +30,7 @@ namespace Aspose.Cells.Examples.RowsColumns.InsertingAndDeleting
 
             //Closing the file stream to free all resources
             fstream.Close();
+            //ExEnd:1
  
         }
     }
