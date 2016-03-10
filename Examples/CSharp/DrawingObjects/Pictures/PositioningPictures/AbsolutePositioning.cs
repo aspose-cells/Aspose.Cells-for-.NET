e950214ceb07da422ef229d26cf928624f4a3b40
Saqib <saqib.masood@aspose.com>
2016-03-10 17:25:58 +0500

    Update AbsolutePositioning.cs

--- Examples/CSharp/DrawingObjects/Pictures/PositioningPictures/AbsolutePositioning.cs
+++ Examples/CSharp/DrawingObjects/Pictures/PositioningPictures/AbsolutePositioning.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.DrawingObjects.Pictures.PositioningPictures
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -33,6 +34,7 @@ namespace Aspose.Cells.Examples.DrawingObjects.Pictures.PositioningPictures
 
             //Saving the Excel file
             workbook.Save(dataDir + "book1.out.xls");
+            //ExEnd:1
 
         }
     }
