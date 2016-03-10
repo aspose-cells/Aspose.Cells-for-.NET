d398907c14ec124eac55289f9b416b3bcf874e8d
Saqib <saqib.masood@aspose.com>
2016-03-10 16:37:33 +0500

    Update RemovingWorksheetsUsingSheetIndex.cs

--- Examples/CSharp/Worksheets/Management/RemovingWorksheetsUsingSheetIndex.cs
+++ Examples/CSharp/Worksheets/Management/RemovingWorksheetsUsingSheetIndex.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Worksheets.Management
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -23,6 +24,7 @@ namespace Aspose.Cells.Examples.Worksheets.Management
 
             //Save workbook
             workbook.Save(dataDir + "output.out.xls");
+            //ExEnd:1
             
         }
     }
