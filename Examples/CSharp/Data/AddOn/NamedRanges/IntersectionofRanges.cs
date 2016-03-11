f6da160b16db5d05a0269215d32d875c075316d7
Saqib <saqib.masood@aspose.com>
2016-03-11 17:50:19 +0500

    Update IntersectionofRanges.cs

--- Examples/CSharp/Data/AddOn/NamedRanges/IntersectionofRanges.cs
+++ Examples/CSharp/Data/AddOn/NamedRanges/IntersectionofRanges.cs
@@ -9,6 +9,7 @@ namespace Aspose.Cells.Examples.Data.AddOn.NamedRanges
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -51,6 +52,7 @@ namespace Aspose.Cells.Examples.Data.AddOn.NamedRanges
 
             //Save the excel file.
             workbook.Save(dataDir + "rngIntersection.out.xls");
+            //ExEnd:1
         }
     }
 }
