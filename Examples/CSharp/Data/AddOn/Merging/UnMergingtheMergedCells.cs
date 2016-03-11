f8e8e79ff2828368a066406f5f327b866c25fe58
Saqib <saqib.masood@aspose.com>
2016-03-11 17:40:19 +0500

    Update UnMergingtheMergedCells.cs

--- Examples/CSharp/Data/AddOn/Merging/UnMergingtheMergedCells.cs
+++ Examples/CSharp/Data/AddOn/Merging/UnMergingtheMergedCells.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Data.AddOn.Merging
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -26,6 +27,7 @@ namespace Aspose.Cells.Examples.Data.AddOn.Merging
 
             //Save the file.
             wbk.Save(dataDir + "unmergingcells.out.xls");
+            //ExEnd:1
 
 
         }
