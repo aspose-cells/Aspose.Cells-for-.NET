da92d03f23035217484d5885410297aef15b360c
Saqib <saqib.masood@aspose.com>
2016-03-11 16:55:40 +0500

    Update AddingComboBoxControl.cs

--- Examples/CSharp/DrawingObjects/Controls/AddingComboBoxControl.cs
+++ Examples/CSharp/DrawingObjects/Controls/AddingComboBoxControl.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.DrawingObjects.Controls
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -42,6 +43,7 @@ namespace Aspose.Cells.Examples.DrawingObjects.Controls
 
             //Add a new combo box.
             Aspose.Cells.Drawing.ComboBox comboBox = sheet.Shapes.AddComboBox(2, 0, 2, 0, 22, 100);
+            //ExEnd:1
 
             //Set the linked cell;
             comboBox.LinkedCell = "A1";
