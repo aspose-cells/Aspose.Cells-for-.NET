099da335d76897596db2fdd7a3020484986d5227
Saqib <saqib.masood@aspose.com>
2016-03-11 18:02:32 +0500

    Update FindingDataOrFormulasUsingFindOptions.cs

--- Examples/CSharp/Data/Handling/Find/FindingDataOrFormulasUsingFindOptions.cs
+++ Examples/CSharp/Data/Handling/Find/FindingDataOrFormulasUsingFindOptions.cs
@@ -9,6 +9,7 @@ namespace Aspose.Cells.Examples.Data.Handling.Find
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -52,7 +53,7 @@ namespace Aspose.Cells.Examples.Data.Handling.Find
             {
                 Console.WriteLine("Record not found ");
             }
-            
+            //ExEnd:1
         }
     }
 }
