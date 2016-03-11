e70ffa07a8836174cbe653d7519a0bf126f456ac
Saqib <saqib.masood@aspose.com>
2016-03-11 13:07:25 +0500

    Update SettingFontStyle.cs

--- Examples/CSharp/Formatting/DealingWithFontSettings/SettingFontStyle.cs
+++ Examples/CSharp/Formatting/DealingWithFontSettings/SettingFontStyle.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Formatting.DealingWithFontSettings
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -42,6 +43,7 @@ namespace Aspose.Cells.Examples.Formatting.DealingWithFontSettings
 
             //Saving the Excel file
             workbook.Save(dataDir + "book1.out.xls", SaveFormat.Excel97To2003);
+            //ExEnd:1
 
             
         }
