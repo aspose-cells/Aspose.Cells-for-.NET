eecae24d8ca2d6e959fa7a8f11038c8cda151419
Saqib <saqib.masood@aspose.com>
2016-03-11 17:52:21 +0500

    Update RenameNamedRange.cs

--- Examples/CSharp/Data/AddOn/NamedRanges/RenameNamedRange.cs
+++ Examples/CSharp/Data/AddOn/NamedRanges/RenameNamedRange.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Data.AddOn.NamedRanges
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -28,6 +29,7 @@ namespace Aspose.Cells.Examples.Data.AddOn.NamedRanges
 
             //Save the Excel file
             workbook.Save(dataDir + "RenamingRange.out.xlsx"); 
+            //ExEnd:1
         }
     }
 }
