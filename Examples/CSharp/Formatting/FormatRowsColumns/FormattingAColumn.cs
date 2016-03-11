07b3380a94069c796018a8e150dfa4aa3a150acb
Saqib <saqib.masood@aspose.com>
2016-03-11 13:12:03 +0500

    Update FormattingAColumn.cs

--- Examples/CSharp/Formatting/FormatRowsColumns/FormattingAColumn.cs
+++ Examples/CSharp/Formatting/FormatRowsColumns/FormattingAColumn.cs
@@ -9,6 +9,7 @@ namespace Aspose.Cells.Examples.Formatting.FormatRowsColumns
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -63,6 +64,7 @@ namespace Aspose.Cells.Examples.Formatting.FormatRowsColumns
 
             //Saving the Excel file
             workbook.Save(dataDir + "book1.out.xls");
+            //ExEnd:1
         }
     }
 }
