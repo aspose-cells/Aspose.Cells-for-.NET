949bb63a128e4e9897d80f79f67457cd5a6c5603
Saqib <saqib.masood@aspose.com>
2016-03-11 16:56:40 +0500

    Update AddingLabelControl.cs

--- Examples/CSharp/DrawingObjects/Controls/AddingLabelControl.cs
+++ Examples/CSharp/DrawingObjects/Controls/AddingLabelControl.cs
@@ -10,6 +10,7 @@ namespace Aspose.Cells.Examples.DrawingObjects.Controls
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -39,6 +40,7 @@ namespace Aspose.Cells.Examples.DrawingObjects.Controls
 
             //Saves the file.
             workbook.Save(dataDir + "book1.out.xls");
+            //ExEnd:1
 
         }
     }
