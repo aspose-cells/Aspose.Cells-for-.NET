4f09066cc71fce12fc2d6539a7263d78f47bea55
Saqib <saqib.masood@aspose.com>
2016-03-11 17:25:48 +0500

    Update AddingLinkToExternalFile.cs

--- Examples/CSharp/Data/AddOn/Hyperlinks/AddingLinkToExternalFile.cs
+++ Examples/CSharp/Data/AddOn/Hyperlinks/AddingLinkToExternalFile.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Data.AddOn.Hyperlinks
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -26,6 +27,7 @@ namespace Aspose.Cells.Examples.Data.AddOn.Hyperlinks
 
             //Saving the Excel file
             workbook.Save(dataDir + "output.out.xls");
+            //ExEnd:1
 
         }
     }
