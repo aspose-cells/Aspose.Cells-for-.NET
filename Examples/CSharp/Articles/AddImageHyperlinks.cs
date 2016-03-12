99b8222cbaa9a390a4602901ccce7155f52786f3
Saqib <saqib.masood@aspose.com>
2016-03-12 12:01:31 +0500

    Update AddImageHyperlinks.cs

--- Examples/CSharp/Articles/AddImageHyperlinks.cs
+++ Examples/CSharp/Articles/AddImageHyperlinks.cs
@@ -9,6 +9,7 @@ namespace Aspose.Cells.Examples.Articles
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -49,6 +50,7 @@ namespace Aspose.Cells.Examples.Articles
 
             //Save the Excel file
             workbook.Save(dataDir+ "ImageHyperlink.out.xls");
+            //ExEnd:1
             
             
         }
