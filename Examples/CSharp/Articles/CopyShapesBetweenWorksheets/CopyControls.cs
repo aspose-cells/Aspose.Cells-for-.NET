5017017a26ab4ae2b8e8164fd5a0206b0303db43
Saqib <saqib.masood@aspose.com>
2016-03-12 11:48:53 +0500

    Update CopyControls.cs

--- Examples/CSharp/Articles/CopyShapesBetweenWorksheets/CopyControls.cs
+++ Examples/CSharp/Articles/CopyShapesBetweenWorksheets/CopyControls.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Articles.CopyShapesBetweenWorksheets
     {
         public static void Main()
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -26,6 +27,7 @@ namespace Aspose.Cells.Examples.Articles.CopyShapesBetweenWorksheets
 
             //Save the Worksheet
             workbook.Save(dataDir+ "Controls.out.xlsx");
+            //ExEnd:1
             
         }
     }
