0a2934f15ce2c8a67daf644b94a1b873109e9223
Saqib <saqib.masood@aspose.com>
2016-03-12 14:12:07 +0500

    Create OpeningEncryptedExcelFiles.cs

---
+++ Examples/CSharp/Files/Handling/OpeningEncryptedExcelFiles.cs
@@ -0,0 +1,28 @@
+using System.IO;
+
+using Aspose.Cells;
+using System;
+
+namespace Aspose.Cells.Examples.Files.Handling
+{
+    public class OpeningEncryptedExcelFiles
+    {
+        public static void Main(string[] args)
+        {
+            //Exstart:1
+            // The path to the documents directory.
+            string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            //Instantiate LoadOptions
+            LoadOptions loadOptions6 = new LoadOptions();
+
+            //Specify the password
+            loadOptions6.Password = "1234";
+
+            //Create a Workbook object and opening the file from its path
+            Workbook wbEncrypted = new Workbook(dataDir + "encryptedBook.xls", loadOptions6);
+            Console.WriteLine("Encrypted excel file opened successfully!");
+            //ExEnd:1
+            }
+          }
+        }
