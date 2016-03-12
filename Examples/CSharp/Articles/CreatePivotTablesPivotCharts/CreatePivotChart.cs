344de78256586085f7cda40101e3dc21f6a2ed03
Saqib <saqib.masood@aspose.com>
2016-03-12 11:50:44 +0500

    Update CreatePivotChart.cs

--- Examples/CSharp/Articles/CreatePivotTablesPivotCharts/CreatePivotChart.cs
+++ Examples/CSharp/Articles/CreatePivotTablesPivotCharts/CreatePivotChart.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Articles.CreatePivotTablesPivotCharts
     {
         public static void Main()
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -25,6 +26,7 @@ namespace Aspose.Cells.Examples.Articles.CreatePivotTablesPivotCharts
             sheet3.Charts[index].HidePivotFieldButtons = false;
             //Saving the Excel file
             workbook.Save(dataDir+ "pivotChart_test.out.xlsx");
+            //ExEnd:1
             
             
         }
