59483f3e03d15d8009dc24090db9cd7995ce3daa
Saqib <saqib.masood@aspose.com>
2016-03-12 11:58:35 +0500

    Update WritingLargeExcelFiles.cs

--- Examples/CSharp/Articles/OptimizingMemoryUsage/WritingLargeExcelFiles.cs
+++ Examples/CSharp/Articles/OptimizingMemoryUsage/WritingLargeExcelFiles.cs
@@ -8,9 +8,10 @@ namespace Aspose.Cells.Examples.Articles.OptimizingMemoryUsage
     {
         public static void Main()
         {
+        //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-            
+            //ExEnd:1
             
         }
     }
