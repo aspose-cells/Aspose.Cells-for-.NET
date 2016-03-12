4400ae197348091e17c3465445648f7ee52faa98
Saqib <saqib.masood@aspose.com>
2016-03-12 10:46:42 +0500

    Update HowToCreateChart.cs

--- Examples/CSharp/Charts/HowToCreateChart.cs
+++ Examples/CSharp/Charts/HowToCreateChart.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Charts
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -54,6 +55,7 @@ namespace Aspose.Cells.Examples.Charts
 
             //Saving the Excel file
             workbook.Save(dataDir + "book1.out.xls");
+            //ExEnd:1
 
         }
     }
