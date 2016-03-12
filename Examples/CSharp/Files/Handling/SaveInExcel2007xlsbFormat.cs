bc7efa579898c3294d64ffb778bde93a2ea9a390
Saqib <saqib.masood@aspose.com>
2016-03-12 15:31:31 +0500

    Update SaveInExcel2007xlsbFormat.cs

--- Examples/CSharp/Files/Handling/SaveInExcel2007xlsbFormat.cs
+++ Examples/CSharp/Files/Handling/SaveInExcel2007xlsbFormat.cs
@@ -8,6 +8,12 @@ namespace Aspose.Cells.Examples.Files.Handling
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
+            // The path to the documents directory.
+            string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            //Creating a Workbook object
+            Workbook workbook = new Workbook();
         //Save in Excel2007 xlsb format
             workbook.Save(dataDir + "book1.out.xlsb", SaveFormat.Xlsb);   
             //ExEnd:1
