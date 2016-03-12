caa76c1e5642a6e0ef9f2140fd82a5419d0e696f
Saqib <saqib.masood@aspose.com>
2016-03-12 12:01:07 +0500

    Update AddConditionalIconsSet.cs

--- Examples/CSharp/Articles/AddConditionalIconsSet.cs
+++ Examples/CSharp/Articles/AddConditionalIconsSet.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Articles
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -85,6 +86,7 @@ namespace Aspose.Cells.Examples.Articles
 
             //Save the Excel file
             workbook.Save(dataDir+ "outfile_cond_icons1.out.xlsx");
+            //ExEnd:1
         }
     }
 }
