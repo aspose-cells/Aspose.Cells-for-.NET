a45b04e3b249e6b8bf076f4c0ea74aa27b84debc
Saqib <saqib.masood@aspose.com>
2016-03-12 10:51:10 +0500

    Update SettingSuperscripteffect.cs

--- Examples/CSharp/Articles/ApplySuperscriptAndSubscript/SettingSuperscripteffect.cs
+++ Examples/CSharp/Articles/ApplySuperscriptAndSubscript/SettingSuperscripteffect.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Articles.ApplySuperscriptAndSubscript
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -38,6 +39,7 @@ namespace Aspose.Cells.Examples.Articles.ApplySuperscriptAndSubscript
 
             //Saving the Excel file
             workbook.Save(dataDir+ "Superscript.out.xls", SaveFormat.Auto);
+            //ExEnd:1
             
             
         }
