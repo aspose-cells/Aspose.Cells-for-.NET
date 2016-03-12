119af7877f5a026dbd9d333be267dd0dbbeb94ba
Saqib <saqib.masood@aspose.com>
2016-03-12 11:40:07 +0500

    Update ConvertWorksheetToImageByPage.cs

--- Examples/CSharp/Articles/ConvertingWorksheetToImage/ConvertWorksheetToImageByPage.cs
+++ Examples/CSharp/Articles/ConvertingWorksheetToImage/ConvertWorksheetToImageByPage.cs
@@ -9,6 +9,7 @@ namespace Aspose.Cells.Examples.Articles.ConvertingWorksheetToImage
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -27,7 +28,7 @@ namespace Aspose.Cells.Examples.Articles.ConvertingWorksheetToImage
                 sr.ToImage(j, dataDir+ "test" + sheet.Name + " Page" + (j + 1) + ".out.tif");
             }
  
-            
+            //ExEnd:1
                        
         }
     }
