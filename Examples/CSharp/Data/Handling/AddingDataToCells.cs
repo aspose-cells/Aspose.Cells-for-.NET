f2ef896c84d8b362a87c27209bb10277a73d51a3
Saqib <saqib.masood@aspose.com>
2016-03-12 10:13:23 +0500

    Update AddingDataToCells.cs

--- Examples/CSharp/Data/Handling/AddingDataToCells.cs
+++ Examples/CSharp/Data/Handling/AddingDataToCells.cs
@@ -9,6 +9,7 @@ namespace Aspose.Cells.Examples.Data.Handling
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -45,6 +46,7 @@ namespace Aspose.Cells.Examples.Data.Handling
 
             //Saving the Excel file
             workbook.Save(dataDir + "output.out.xls");
+            //ExEnd:1
 
         }
     }
