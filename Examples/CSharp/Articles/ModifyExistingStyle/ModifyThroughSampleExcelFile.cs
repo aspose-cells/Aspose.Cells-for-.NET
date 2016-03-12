d5e4b2d5ce0f1cbe860e43e63cd1f9458fda774a
Saqib <saqib.masood@aspose.com>
2016-03-12 11:55:49 +0500

    Update ModifyThroughSampleExcelFile.cs

--- Examples/CSharp/Articles/ModifyExistingStyle/ModifyThroughSampleExcelFile.cs
+++ Examples/CSharp/Articles/ModifyExistingStyle/ModifyThroughSampleExcelFile.cs
@@ -9,6 +9,7 @@ namespace Aspose.Cells.Examples.Articles.ModifyExistingStyle
     {
         public static void Main()
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -32,6 +33,7 @@ namespace Aspose.Cells.Examples.Articles.ModifyExistingStyle
 
             //Save the excel file.	
             workbook.Save(dataDir+ "book2.out.xlsx");
+            //ExEnd:1
             
         }
     }
