4128f4ed9cdb60488e4d962bf9dc6a7393d641e0
Saqib <saqib.masood@aspose.com>
2016-03-12 10:05:28 +0500

    Update ImportingFromArray.cs

--- Examples/CSharp/Data/Handling/Importing/ImportingFromArray.cs
+++ Examples/CSharp/Data/Handling/Importing/ImportingFromArray.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Data.Handling.Importing
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -30,6 +31,7 @@ namespace Aspose.Cells.Examples.Data.Handling.Importing
 
             //Saving the Excel file
             workbook.Save(dataDir + "DataImport.out.xls");
+            //ExEnd:1
 
         }
     }
