827d3341fdebabf5cd9cdd16c3e16022fc32159f
Saqib <saqib.masood@aspose.com>
2016-03-12 10:08:34 +0500

    Update ImportingFromDataTable.cs

--- Examples/CSharp/Data/Handling/Importing/ImportingFromDataTable.cs
+++ Examples/CSharp/Data/Handling/Importing/ImportingFromDataTable.cs
@@ -10,6 +10,7 @@ namespace Aspose.Cells.Examples.Data.Handling.Importing
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -61,6 +62,7 @@ namespace Aspose.Cells.Examples.Data.Handling.Importing
 
             //Saving the Excel file
             workbook.Save(dataDir + "DataImport.out.xls");
+            //ExEnd:1
  
         }
     }
