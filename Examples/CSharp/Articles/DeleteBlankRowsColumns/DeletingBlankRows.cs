4c09f423283e779f564ad7b7ac0d6ad2347d18bf
Saqib <saqib.masood@aspose.com>
2016-03-12 11:52:18 +0500

    Update DeletingBlankRows.cs

--- Examples/CSharp/Articles/DeleteBlankRowsColumns/DeletingBlankRows.cs
+++ Examples/CSharp/Articles/DeleteBlankRowsColumns/DeletingBlankRows.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Articles.DeleteBlankRowsColumns
     {
         public static void Main()
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -27,6 +28,7 @@ namespace Aspose.Cells.Examples.Articles.DeleteBlankRowsColumns
 
             //Save the excel file.
             wb.Save(dataDir+ "mybook.out.xlsx");
+            //ExEnd:1
             
             
         }
