450a5ff4fe7e143061f6fbddf846063716b3291f
Saqib <saqib.masood@aspose.com>
2016-03-12 12:02:08 +0500

    Update AddLibraryReferenceToVbaProject.cs

--- Examples/CSharp/Articles/AddLibraryReferenceToVbaProject.cs
+++ Examples/CSharp/Articles/AddLibraryReferenceToVbaProject.cs
@@ -6,6 +6,7 @@ namespace Aspose.Cells.Examples.Articles
     class AddLibraryReferenceToVbaProject
     {
         static void Main() {
+            //ExStart:1
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
             string outputPath = dataDir + "Output.out.xlsm";
 
@@ -17,6 +18,7 @@ namespace Aspose.Cells.Examples.Articles
             vbaProj.References.AddRegisteredReference("Office", "*\\G{2DF8D04C-5BFA-101B-BDE5-00AA0044DE52}#2.0#0#C:\\Program Files\\Common Files\\Microsoft Shared\\OFFICE14\\MSO.DLL#Microsoft Office 14.0 Object Library");
 
             workbook.Save(outputPath);
+            //ExEnd:1
         }
     }
 }
