f1c7191cf96e3221d041c4c470679a5ac10a5088
Saqib <saqib.masood@aspose.com>
2016-03-12 14:27:09 +0500

    Create SaveFileInExcel97-2003format.cs

---
+++ Examples/CSharp/Files/Handling/SaveFileInExcel97-2003format.cs
@@ -0,0 +1,28 @@
+using System.IO;
+
+using Aspose.Cells;
+
+namespace Aspose.Cells.Examples.Files.Handling
+{
+    public class SavingFiles
+    {
+        public static void Main(string[] args)
+        {
+            //ExStart:1
+            // The path to the documents directory.
+            string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            //Creating a Workbook object
+            Workbook workbook = new Workbook();
+
+            //Your Code goes here for any workbook related operations
+
+            //Save in Excel 97 – 2003 format
+            workbook.Save(dataDir + "book1.out.xls");
+
+            //OR
+            workbook.Save(dataDir + "book1.out.xls", new XlsSaveOptions(SaveFormat.Excel97To2003));
+            //ExEnd:1
+           }
+         }
+      }
