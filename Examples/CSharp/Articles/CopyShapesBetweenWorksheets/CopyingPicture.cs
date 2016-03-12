72dde11979aba1a6a763c7a16b05537fcaa3e22e
Saqib <saqib.masood@aspose.com>
2016-03-12 11:49:32 +0500

    Update CopyingPicture.cs

--- Examples/CSharp/Articles/CopyShapesBetweenWorksheets/CopyingPicture.cs
+++ Examples/CSharp/Articles/CopyShapesBetweenWorksheets/CopyingPicture.cs
@@ -9,6 +9,7 @@ namespace Aspose.Cells.Examples.Articles.CopyShapesBetweenWorksheets
     {
         public static void Main()
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -27,7 +28,7 @@ namespace Aspose.Cells.Examples.Articles.CopyShapesBetweenWorksheets
 
             //Save the Worksheet
             workbook.Save(dataDir+ "Shapes.out.xlsx");
-            
+            //ExEnd:1
             
         }
     }
