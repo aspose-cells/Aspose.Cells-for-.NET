5e15987fbf399f3c2900d384eb23dce3370531e7
Saqib <saqib.masood@aspose.com>
2016-03-14 12:40:16 +0500

    Update LoadSpecificSheets.cs

--- Examples/CSharp/Articles/LoadSpecificSheets.cs
+++ Examples/CSharp/Articles/LoadSpecificSheets.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Articles
     {
         public static void Main()
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
             //Define a new Workbook.
@@ -29,7 +30,7 @@ namespace Aspose.Cells.Examples.Articles
 
             //Save the workbook.
             workbook.Save(dataDir+ "outputFile.out.xlsx");
-            
+            //ExEnd:1
             
         }
     }
