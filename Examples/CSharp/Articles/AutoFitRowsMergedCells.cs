778cf1888bfbd620544e132565951b0b838a75e4
Saqib <saqib.masood@aspose.com>
2016-03-14 11:39:22 +0500

    Update AutoFitRowsMergedCells.cs

--- Examples/CSharp/Articles/AutoFitRowsMergedCells.cs
+++ Examples/CSharp/Articles/AutoFitRowsMergedCells.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Articles
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -51,7 +52,7 @@ namespace Aspose.Cells.Examples.Articles
 
             //Save the Excel file
             wb.Save(dataDir+ "AutoFitMergedCells.out.xlsx");
-            
+            //ExEnd:1
             
         }
     }
