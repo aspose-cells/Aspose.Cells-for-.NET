00c9dbd9e294a0e81036fbf9ace7c5f8776fb9ea
Saqib <saqib.masood@aspose.com>
2016-03-14 11:42:50 +0500

    Update CopyRangeDataOnly.cs

--- Examples/CSharp/Articles/CopyRangeDataOnly.cs
+++ Examples/CSharp/Articles/CopyRangeDataOnly.cs
@@ -9,6 +9,7 @@ namespace Aspose.Cells.Examples.Articles
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -72,6 +73,7 @@ namespace Aspose.Cells.Examples.Articles
 
             //Save the excel file.
             workbook.Save(dataDir+ "CopyRangeData.out.xlsx");
+            //ExEnd:1
             
             
         }
