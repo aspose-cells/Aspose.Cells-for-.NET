f25fa97a8940f6fdb037e8fe2a69fe1a08d51221
Saqib <saqib.masood@aspose.com>
2016-03-14 12:28:45 +0500

    Update InsertLinkedPicture.cs

--- Examples/CSharp/Articles/InsertLinkedPicture.cs
+++ Examples/CSharp/Articles/InsertLinkedPicture.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Articles
     {
         public static void Main()
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
             //Instantiate a new Workbook.
@@ -19,7 +20,7 @@ namespace Aspose.Cells.Examples.Articles
             pic.WidthInch = 2.6;
             //Save the Excel file.
             workbook.Save(dataDir+ "outLinkedPicture.out.xlsx");
-            
+            //ExEnd:1
             
         }
     }
