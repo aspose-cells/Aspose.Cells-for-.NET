ee3e12267bbab7f3a822744867b6eb36d85173a8
Saqib <saqib.masood@aspose.com>
2016-03-14 11:38:06 +0500

    Update AddWordArtWatermarkToWorksheet.cs

--- Examples/CSharp/Articles/AddWordArtWatermarkToWorksheet.cs
+++ Examples/CSharp/Articles/AddWordArtWatermarkToWorksheet.cs
@@ -9,6 +9,7 @@ namespace Aspose.Cells.Examples.Articles
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -43,6 +44,7 @@ namespace Aspose.Cells.Examples.Articles
 
             //Save the file
             workbook.Save(dataDir+ "Watermark_Test.out.xls");
+            //ExEnd:1
             
             
         }
