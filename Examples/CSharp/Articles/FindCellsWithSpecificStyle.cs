2ba6a33184d22dee752ac63df5ef24c42d50e4aa
Saqib <saqib.masood@aspose.com>
2016-03-14 11:51:42 +0500

    Update FindCellsWithSpecificStyle.cs

--- Examples/CSharp/Articles/FindCellsWithSpecificStyle.cs
+++ Examples/CSharp/Articles/FindCellsWithSpecificStyle.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Articles
     {
         public static void Main()
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -40,6 +41,7 @@ namespace Aspose.Cells.Examples.Articles
             } while (true);
 
             workbook.Save(dataDir+ "output.out.xlsx");
+            //ExEnd:1
 
             
         }
