2f9f4803a0aa504b8511c5ca652ba8a2ed360532
Saqib <saqib.masood@aspose.com>
2016-03-14 11:43:38 +0500

    Update CreateManipulateRemoveScenarios.cs

--- Examples/CSharp/Articles/CreateManipulateRemoveScenarios.cs
+++ Examples/CSharp/Articles/CreateManipulateRemoveScenarios.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Articles
     {
         public static void Main()
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
             //Instantiate the Workbook
@@ -34,6 +35,7 @@ namespace Aspose.Cells.Examples.Articles
 
             //Save the Excel file.
             workbook.Save(dataDir+ "outBk_scenarios1.out.xlsx");
+            //ExEnd:1
             
         }
     }
