5aa0789f7cb41c4f6a9cc5c4bd9c5fc08b97244a
Saqib <saqib.masood@aspose.com>
2016-03-14 11:37:47 +0500

    Update AddPictureToExcelComment.cs

--- Examples/CSharp/Articles/AddPictureToExcelComment.cs
+++ Examples/CSharp/Articles/AddPictureToExcelComment.cs
@@ -9,6 +9,7 @@ namespace Aspose.Cells.Examples.Articles
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -40,7 +41,7 @@ namespace Aspose.Cells.Examples.Articles
             //Save the workbook
             workbook.Save(dataDir+ "commentwithpicture1.out.xlsx", Aspose.Cells.SaveFormat.Xlsx);
             
-            
+            //ExEnd:1
         }
     }
 }
