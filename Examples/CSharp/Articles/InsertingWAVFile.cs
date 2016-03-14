d76e188d28657629f7c61b67953c77f52b8ecb17
Saqib <saqib.masood@aspose.com>
2016-03-14 12:29:17 +0500

    Update InsertingWAVFile.cs

--- Examples/CSharp/Articles/InsertingWAVFile.cs
+++ Examples/CSharp/Articles/InsertingWAVFile.cs
@@ -10,6 +10,7 @@ namespace Aspose.Cells.Examples.Articles
     {
         public static void Main()
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
             //Define a string variable to store the image path.
@@ -56,6 +57,7 @@ namespace Aspose.Cells.Examples.Articles
 
             //Save the excel file
             workbook.Save(dataDir+ "testWAV.out.xlsx"); 
+            //ExEnd:1
             
             
         }
