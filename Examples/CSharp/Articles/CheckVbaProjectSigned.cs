74c187114a86716d9d5c35194e8202c3c3c04484
Saqib <saqib.masood@aspose.com>
2016-03-14 11:40:15 +0500

    Update CheckVbaProjectSigned.cs

--- Examples/CSharp/Articles/CheckVbaProjectSigned.cs
+++ Examples/CSharp/Articles/CheckVbaProjectSigned.cs
@@ -6,11 +6,13 @@ namespace Aspose.Cells.Examples.Articles
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
             Workbook workbook = new Workbook(dataDir + "Sample1.xlsx");
             Console.WriteLine("VBA Project is Signed: " + workbook.VbaProject.IsSigned);
+            //ExEnd:1
         }
     }
 }
