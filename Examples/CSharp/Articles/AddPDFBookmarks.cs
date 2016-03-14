b0cb11cb5225bbdc6f2c6f8f955f9627dcfd0521
Saqib <saqib.masood@aspose.com>
2016-03-14 11:37:22 +0500

    Update AddPDFBookmarks.cs

--- Examples/CSharp/Articles/AddPDFBookmarks.cs
+++ Examples/CSharp/Articles/AddPDFBookmarks.cs
@@ -9,6 +9,7 @@ namespace Aspose.Cells.Examples.Articles
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -105,6 +106,7 @@ namespace Aspose.Cells.Examples.Articles
 
             //Save the pdf file
             workbook.Save(dataDir+ "PDFBookmarks_test.out.pdf", pdfSaveOptions);
+            //ExEnd:1
             
             
         }
