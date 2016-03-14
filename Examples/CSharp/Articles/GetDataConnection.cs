82996ee40ae31d7049110fcdb59d6f9da3aef4cd
Saqib <saqib.masood@aspose.com>
2016-03-14 12:26:30 +0500

    Update GetDataConnection.cs

--- Examples/CSharp/Articles/GetDataConnection.cs
+++ Examples/CSharp/Articles/GetDataConnection.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Articles
     {
         public static void Main(string[] args)
         {
+                //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -21,5 +22,6 @@ namespace Aspose.Cells.Examples.Articles
                 Console.WriteLine("Web Query URL: " + webQuery.Url);
             }
         }
+            //ExEnd:1
     }
 }
