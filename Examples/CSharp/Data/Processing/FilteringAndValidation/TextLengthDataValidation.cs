6c8b234ab74ab1efa91e2e51a814654ef81a2125
Saqib <saqib.masood@aspose.com>
2016-03-19 14:36:27 +0500

    Update TextLengthDataValidation.cs

--- Examples/CSharp/Data/Processing/FilteringAndValidation/TextLengthDataValidation.cs
+++ Examples/CSharp/Data/Processing/FilteringAndValidation/TextLengthDataValidation.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Data.Processing.FilteringAndValidation
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -74,7 +75,7 @@ namespace Aspose.Cells.Examples.Data.Processing.FilteringAndValidation
 
             // Save the Excel file.
             workbook.Save(dataDir + "output.out.xls");
- 
+            //ExEnd:1
         }
     }
 }
