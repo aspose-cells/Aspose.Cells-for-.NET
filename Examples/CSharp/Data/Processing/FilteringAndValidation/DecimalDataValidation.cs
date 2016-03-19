2fac527bd64538a56a1bf5cbd472d4b58342a6b2
Saqib <saqib.masood@aspose.com>
2016-03-19 14:35:25 +0500

    Update DecimalDataValidation.cs

--- Examples/CSharp/Data/Processing/FilteringAndValidation/DecimalDataValidation.cs
+++ Examples/CSharp/Data/Processing/FilteringAndValidation/DecimalDataValidation.cs
@@ -9,6 +9,7 @@ namespace Aspose.Cells.Examples.Data.Processing.Processing.FilteringAndValidatio
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -54,6 +55,7 @@ namespace Aspose.Cells.Examples.Data.Processing.Processing.FilteringAndValidatio
 
             // Save the workbook.
             workbook.Save(dataDir + "output.out.xls");
+            //ExEnd:1
 
         }
     }
