516318e24956a5540f0a5c3ff7b214b95ca46935
Saqib <saqib.masood@aspose.com>
2016-03-19 14:37:01 +0500

    Update WholeNumberDataValidation.cs

--- Examples/CSharp/Data/Processing/FilteringAndValidation/WholeNumberDataValidation.cs
+++ Examples/CSharp/Data/Processing/FilteringAndValidation/WholeNumberDataValidation.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Data.Processing.FilteringAndValidation
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -54,6 +55,7 @@ namespace Aspose.Cells.Examples.Data.Processing.FilteringAndValidation
 
             // Save the workbook.
             workbook.Save(dataDir + "output.out.xls");
+            //ExEnd:1
 
             
         }
