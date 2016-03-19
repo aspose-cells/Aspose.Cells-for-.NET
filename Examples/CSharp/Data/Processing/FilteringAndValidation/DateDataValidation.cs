432aab35bf856fb0621997ce1f4ee62c39686efe
Saqib <saqib.masood@aspose.com>
2016-03-19 14:34:47 +0500

    Update DateDataValidation.cs

--- Examples/CSharp/Data/Processing/FilteringAndValidation/DateDataValidation.cs
+++ Examples/CSharp/Data/Processing/FilteringAndValidation/DateDataValidation.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Data.Processing.Processing.FilteringAndValidatio
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -76,6 +77,7 @@ namespace Aspose.Cells.Examples.Data.Processing.Processing.FilteringAndValidatio
 
             // Save the Excel file.
             workbook.Save(dataDir + "output.out.xls");
+            //ExEnd:1
 
         }
     }
