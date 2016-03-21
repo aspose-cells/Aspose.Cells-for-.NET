db52eec58f1eb9482632f0bced98410a371c66a8
Saqib <saqib.masood@aspose.com>
2016-03-21 14:46:24 +0500

    Update CreatingSubtotals.cs

--- Examples/CSharp/Data/Processing/CreatingSubtotals.cs
+++ Examples/CSharp/Data/Processing/CreatingSubtotals.cs
@@ -8,6 +8,7 @@ namespace Aspose.Cells.Examples.Data.Processing
     {
         public static void Main(string[] args)
         {
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -31,6 +32,7 @@ namespace Aspose.Cells.Examples.Data.Processing
 
             //Save the excel file
             workbook.Save(dataDir + "output.out.xls");
+            //ExEnd:1
 
         }
     }
