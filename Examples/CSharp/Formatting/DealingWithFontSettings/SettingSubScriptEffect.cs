0cc8748d1df0dd247d150462d27ac2071d1aa93f
Saqib <saqib.masood@aspose.com>
2016-03-26 15:22:55 +0500

    Update SettingSubScriptEffect.cs

--- Examples/CSharp/Formatting/DealingWithFontSettings/SettingSubScriptEffect.cs
+++ Examples/CSharp/Formatting/DealingWithFontSettings/SettingSubScriptEffect.cs
@@ -8,7 +8,7 @@ namespace Aspose.Cells.Examples.Formatting.DealingWithFontSettings
     {
         public static void Main(string[] args)
         {
-            //Exstart:1
+            //ExStart:1
             // The path to the documents directory.
             string dataDir = Aspose.Cells.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
