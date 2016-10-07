343f4bcce99e99816334d0204cb097dd357cdc85
Asad Ali <asad.ali@aspose.com>
2016-10-07 12:31:27 +0500

    Added VisualBasic Example For GridDesktop

---
+++ Examples.GridDesktop/VisualBasic/UsingGridDesktopInWpf/MainWindow.xaml.vb
@@ -0,0 +1,17 @@
+﻿Imports System.IO
+
+Class MainWindow
+
+    Private Sub FrameworkElement_OnLoaded(sender As System.Object, e As System.Windows.RoutedEventArgs)
+        ' ExStart:UsingGridDesktopInWpf
+        ' The path to the documents directory.
+        Dim dataDir As String = Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType)
+
+        Using stream = New MemoryStream(File.ReadAllBytes(dataDir & Convert.ToString("SampleBook.xlsx")))
+            Me.grid.ImportExcelFile(stream)
+            Me.grid.ExportExcelFile(dataDir & Convert.ToString("SampleOutput_out_.xlsx"))
+        End Using
+        ' ExEnd:UsingGridDesktopInWpf
+    End Sub
+
+End Class
---
+++ Examples.GridDesktop/VisualBasic/UsingGridDesktopInWpf/Utils.vb
@@ -0,0 +1,23 @@
+﻿Imports System.IO
+
+Public Class Utils
+    Public Shared Function GetDataDir(t As Type) As String
+        Dim retDir As String = Nothing
+        Dim parent = Directory.GetParent(Directory.GetCurrentDirectory()).Parent
+        Dim startDirectory As String = Nothing
+        If parent IsNot Nothing Then
+            Dim directoryInfo = parent.Parent
+            If directoryInfo IsNot Nothing Then
+                startDirectory = directoryInfo.FullName
+            End If
+            retDir = Path.Combine(Path.GetFullPath(Path.Combine(startDirectory, "..\")), "Data\") + parent.Name
+
+            If Not Directory.Exists(retDir) Then
+                Directory.CreateDirectory(retDir)
+            End If
+        Else
+            retDir = parent.FullName
+        End If
+        Return retDir & Convert.ToString("\")
+    End Function
+End Class
