21dfafda84830b0bd4d87f3b65322a6a0c4a3583
Asad Ali <asad.ali@aspose.com>
2016-11-15 20:21:22 +0500

    Added New GridDesktop Examples

---
+++ Examples.GridDesktop/CSharp/GridDesktop.Examples/RunExamples.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace GridDesktop.Examples
+{
+    partial class RunExamples
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.articlesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyAndPasteRowsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataBindingFeaturesInWorksheetsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.workingWithGridToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.managingContextMenuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openingExcelFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.savingExcelFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gridDesktopEventsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.workingWithWorksheetsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.accessingWorksheetsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.workingWithRowsAndColumnsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.workingWithCellsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addORInsertWorksheetsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeAWorksheetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.renameAWorksheetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importDataFromDataTableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.articlesToolStripMenuItem,
+            this.workingWithGridToolStripMenuItem,
+            this.workingWithWorksheetsToolStripMenuItem,
+            this.workingWithRowsAndColumnsToolStripMenuItem,
+            this.workingWithCellsToolStripMenuItem,
+            this.closeAllToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(854, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // articlesToolStripMenuItem
+            // 
+            this.articlesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyAndPasteRowsToolStripMenuItem,
+            this.dataBindingFeaturesInWorksheetsToolStripMenuItem});
+            this.articlesToolStripMenuItem.Name = "articlesToolStripMenuItem";
+            this.articlesToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.articlesToolStripMenuItem.Text = "Articles";
+            // 
+            // copyAndPasteRowsToolStripMenuItem
+            // 
+            this.copyAndPasteRowsToolStripMenuItem.Name = "copyAndPasteRowsToolStripMenuItem";
+            this.copyAndPasteRowsToolStripMenuItem.Size = new System.Drawing.Size(266, 22);
+            this.copyAndPasteRowsToolStripMenuItem.Text = "Copy And Paste Rows";
+            this.copyAndPasteRowsToolStripMenuItem.Click += new System.EventHandler(this.copyAndPasteRowsToolStripMenuItem_Click);
+            // 
+            // dataBindingFeaturesInWorksheetsToolStripMenuItem
+            // 
+            this.dataBindingFeaturesInWorksheetsToolStripMenuItem.Name = "dataBindingFeaturesInWorksheetsToolStripMenuItem";
+            this.dataBindingFeaturesInWorksheetsToolStripMenuItem.Size = new System.Drawing.Size(266, 22);
+            this.dataBindingFeaturesInWorksheetsToolStripMenuItem.Text = "Data Binding Features in Worksheets";
+            this.dataBindingFeaturesInWorksheetsToolStripMenuItem.Click += new System.EventHandler(this.dataBindingFeaturesInWorksheetsToolStripMenuItem_Click);
+            // 
+            // workingWithGridToolStripMenuItem
+            // 
+            this.workingWithGridToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.managingContextMenuToolStripMenuItem,
+            this.openingExcelFileToolStripMenuItem,
+            this.savingExcelFileToolStripMenuItem,
+            this.gridDesktopEventsToolStripMenuItem});
+            this.workingWithGridToolStripMenuItem.Name = "workingWithGridToolStripMenuItem";
+            this.workingWithGridToolStripMenuItem.Size = new System.Drawing.Size(117, 20);
+            this.workingWithGridToolStripMenuItem.Text = "Working With Grid";
+            // 
+            // managingContextMenuToolStripMenuItem
+            // 
+            this.managingContextMenuToolStripMenuItem.Name = "managingContextMenuToolStripMenuItem";
+            this.managingContextMenuToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.managingContextMenuToolStripMenuItem.Text = "Managing Context Menu";
+            this.managingContextMenuToolStripMenuItem.Click += new System.EventHandler(this.managingContextMenuToolStripMenuItem_Click);
+            // 
+            // openingExcelFileToolStripMenuItem
+            // 
+            this.openingExcelFileToolStripMenuItem.Name = "openingExcelFileToolStripMenuItem";
+            this.openingExcelFileToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.openingExcelFileToolStripMenuItem.Text = "Opening Excel File";
+            this.openingExcelFileToolStripMenuItem.Click += new System.EventHandler(this.openingExcelFileToolStripMenuItem_Click);
+            // 
+            // savingExcelFileToolStripMenuItem
+            // 
+            this.savingExcelFileToolStripMenuItem.Name = "savingExcelFileToolStripMenuItem";
+            this.savingExcelFileToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.savingExcelFileToolStripMenuItem.Text = "Saving Excel File";
+            this.savingExcelFileToolStripMenuItem.Click += new System.EventHandler(this.savingExcelFileToolStripMenuItem_Click);
+            // 
+            // gridDesktopEventsToolStripMenuItem
+            // 
+            this.gridDesktopEventsToolStripMenuItem.Name = "gridDesktopEventsToolStripMenuItem";
+            this.gridDesktopEventsToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.gridDesktopEventsToolStripMenuItem.Text = "GridDesktop Events";
+            this.gridDesktopEventsToolStripMenuItem.Click += new System.EventHandler(this.gridDesktopEventsToolStripMenuItem_Click);
+            // 
+            // workingWithWorksheetsToolStripMenuItem
+            // 
+            this.workingWithWorksheetsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.accessingWorksheetsToolStripMenuItem,
+            this.addORInsertWorksheetsToolStripMenuItem,
+            this.removeAWorksheetToolStripMenuItem,
+            this.renameAWorksheetToolStripMenuItem,
+            this.importDataFromDataTableToolStripMenuItem});
+            this.workingWithWorksheetsToolStripMenuItem.Name = "workingWithWorksheetsToolStripMenuItem";
+            this.workingWithWorksheetsToolStripMenuItem.Size = new System.Drawing.Size(151, 20);
+            this.workingWithWorksheetsToolStripMenuItem.Text = "Working With Worksheet";
+            // 
+            // accessingWorksheetsToolStripMenuItem
+            // 
+            this.accessingWorksheetsToolStripMenuItem.Name = "accessingWorksheetsToolStripMenuItem";
+            this.accessingWorksheetsToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.accessingWorksheetsToolStripMenuItem.Text = "Accessing Worksheets";
+            this.accessingWorksheetsToolStripMenuItem.Click += new System.EventHandler(this.accessingWorksheetsToolStripMenuItem_Click);
+            // 
+            // workingWithRowsAndColumnsToolStripMenuItem
+            // 
+            this.workingWithRowsAndColumnsToolStripMenuItem.Name = "workingWithRowsAndColumnsToolStripMenuItem";
+            this.workingWithRowsAndColumnsToolStripMenuItem.Size = new System.Drawing.Size(197, 20);
+            this.workingWithRowsAndColumnsToolStripMenuItem.Text = "Working With Rows and Columns";
+            // 
+            // workingWithCellsToolStripMenuItem
+            // 
+            this.workingWithCellsToolStripMenuItem.Name = "workingWithCellsToolStripMenuItem";
+            this.workingWithCellsToolStripMenuItem.Size = new System.Drawing.Size(120, 20);
+            this.workingWithCellsToolStripMenuItem.Text = "Working With Cells";
+            // 
+            // closeAllToolStripMenuItem
+            // 
+            this.closeAllToolStripMenuItem.ForeColor = System.Drawing.Color.Navy;
+            this.closeAllToolStripMenuItem.Name = "closeAllToolStripMenuItem";
+            this.closeAllToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.closeAllToolStripMenuItem.Text = "Close All";
+            this.closeAllToolStripMenuItem.Click += new System.EventHandler(this.closeAllToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.ForeColor = System.Drawing.Color.Red;
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // addORInsertWorksheetsToolStripMenuItem
+            // 
+            this.addORInsertWorksheetsToolStripMenuItem.Name = "addORInsertWorksheetsToolStripMenuItem";
+            this.addORInsertWorksheetsToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.addORInsertWorksheetsToolStripMenuItem.Text = "Add OR Insert Worksheets";
+            this.addORInsertWorksheetsToolStripMenuItem.Click += new System.EventHandler(this.addORInsertWorksheetsToolStripMenuItem_Click);
+            // 
+            // removeAWorksheetToolStripMenuItem
+            // 
+            this.removeAWorksheetToolStripMenuItem.Name = "removeAWorksheetToolStripMenuItem";
+            this.removeAWorksheetToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.removeAWorksheetToolStripMenuItem.Text = "Remove A Worksheet";
+            this.removeAWorksheetToolStripMenuItem.Click += new System.EventHandler(this.removeAWorksheetToolStripMenuItem_Click);
+            // 
+            // renameAWorksheetToolStripMenuItem
+            // 
+            this.renameAWorksheetToolStripMenuItem.Name = "renameAWorksheetToolStripMenuItem";
+            this.renameAWorksheetToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.renameAWorksheetToolStripMenuItem.Text = "Rename A Worksheet";
+            this.renameAWorksheetToolStripMenuItem.Click += new System.EventHandler(this.renameAWorksheetToolStripMenuItem_Click);
+            // 
+            // importDataFromDataTableToolStripMenuItem
+            // 
+            this.importDataFromDataTableToolStripMenuItem.Name = "importDataFromDataTableToolStripMenuItem";
+            this.importDataFromDataTableToolStripMenuItem.Size = new System.Drawing.Size(223, 22);
+            this.importDataFromDataTableToolStripMenuItem.Text = "Import Data From DataTable";
+            this.importDataFromDataTableToolStripMenuItem.Click += new System.EventHandler(this.importDataFromDataTableToolStripMenuItem_Click);
+            // 
+            // RunExamples
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 530);
+            this.Controls.Add(this.menuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "RunExamples";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aspose.Cells for .NET GridDesktop Examples";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem articlesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem workingWithGridToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem workingWithWorksheetsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem workingWithRowsAndColumnsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem workingWithCellsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyAndPasteRowsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dataBindingFeaturesInWorksheetsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem managingContextMenuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openingExcelFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem savingExcelFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gridDesktopEventsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem accessingWorksheetsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addORInsertWorksheetsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeAWorksheetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem renameAWorksheetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importDataFromDataTableToolStripMenuItem;
+    }
+}
+
---
+++ Examples.GridDesktop/CSharp/GridDesktop.Examples/RunExamples.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GridDesktop.Examples.WorkingWithGrid;
+using GridDesktop.Examples.Articles;
+using GridDesktop.Examples.WorkingWithWorksheet;
+
+namespace GridDesktop.Examples
+{
+    public partial class RunExamples : Form
+    {
+        public RunExamples()
+        {
+            InitializeComponent();
+        }
+
+        private void closeAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Form frm in this.MdiChildren)
+            {
+                if (frm != this)
+                {
+                    frm.Close();
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void copyAndPasteRowsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CopyAndPasteRows frmCopyPasteRows = new CopyAndPasteRows();
+            frmCopyPasteRows.MdiParent = this;
+            frmCopyPasteRows.Show();
+        }
+
+        private void dataBindingFeaturesInWorksheetsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DataBindingFeature frmDataBindingFeatures = new DataBindingFeature();
+            frmDataBindingFeatures.MdiParent = this;
+            frmDataBindingFeatures.Show();
+        }
+
+        private void managingContextMenuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ManagingContextMenu frmManagingContextMenu = new ManagingContextMenu();
+            frmManagingContextMenu.MdiParent = this;
+            frmManagingContextMenu.Show();
+        }
+
+        private void openingExcelFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpeningExcelFile frmOpeningExcelFile = new OpeningExcelFile();
+            frmOpeningExcelFile.MdiParent = this;
+            frmOpeningExcelFile.Show();
+        }
+
+        private void savingExcelFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SavingExcelFile frmSavingExcelFile = new SavingExcelFile();
+            frmSavingExcelFile.MdiParent = this;
+            frmSavingExcelFile.Show();
+        }
+
+        private void gridDesktopEventsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GridDesktopEvents frmGridDesktopEvents = new GridDesktopEvents();
+            frmGridDesktopEvents.MdiParent = this;
+            frmGridDesktopEvents.Show();
+        }
+
+        private void accessingWorksheetsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AccessingWorksheets frmAccessingWorksheets = new AccessingWorksheets();
+            frmAccessingWorksheets.MdiParent = this;
+            frmAccessingWorksheets.Show();
+        }
+
+        private void addORInsertWorksheetsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddInsertWorksheet frmAddInsertWorksheet = new AddInsertWorksheet();
+            frmAddInsertWorksheet.MdiParent = this;
+            frmAddInsertWorksheet.Show();
+        }
+
+        private void removeAWorksheetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RemoveWorksheet frmRemoveWorksheet = new RemoveWorksheet();
+            frmRemoveWorksheet.MdiParent = this;
+            frmRemoveWorksheet.Show();
+        }
+
+        private void renameAWorksheetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RenameWorksheet frmRenameWorksheet = new RenameWorksheet();
+            frmRenameWorksheet.MdiParent = this;
+            frmRenameWorksheet.Show();
+        }
+
+        private void importDataFromDataTableToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ImportDataFromDataTable frmImportData = new ImportDataFromDataTable();
+            frmImportData.MdiParent = this;
+            frmImportData.Show();
+        }
+    }
+}
