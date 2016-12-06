08fd208024ec48d8932ee7f9742054fd65a6d420
Asad Ali <asad.ali@aspose.com>
2016-12-06 15:57:26 +0500

    Added missing knowledge based Examples

---
+++ Examples/VisualBasic/KnowledgeBase/ComparingVSTOWithAspose/UsingAsposeCells.vb
@@ -0,0 +1,26 @@
+﻿Namespace KnowledgeBase.ComparingVSTOWithAspose
+    Public Class UsingAsposeCells
+        Public Shared Sub Run()
+            ' ExStart:1
+            ' The path to the documents directory.
+            Dim dataDir As String = RunExamples.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType)
+
+            Dim start As DateTime = DateTime.Now
+            Dim myPath As String = dataDir & Convert.ToString("TempBook.xls")
+            Dim workbook As New Workbook(myPath)
+            Dim ws As Worksheet = workbook.Worksheets(0)
+
+            For i As Integer = 0 To 999
+                For j As Integer = 0 To 19
+                    ws.Cells(i, j).PutValue("Row " + (i + 1).ToString() + " " + "Col " + (j + 1).ToString())
+                Next
+            Next
+
+            workbook.Save(dataDir & Convert.ToString("TempBook1.xls"))
+            Dim [end] As DateTime = DateTime.Now
+            Dim time As TimeSpan = [end] - start
+            Console.WriteLine("File Created! " + "Time consumed (Seconds): " + time.TotalSeconds.ToString())
+            ' ExEnd:1
+        End Sub
+    End Class
+End Namespace
---
+++ Examples/VisualBasic/KnowledgeBase/ComparingVSTOWithAspose/VSTOCode.vb
@@ -0,0 +1,37 @@
+﻿Imports Microsoft.Office.Interop
+Imports Microsoft.Office.Interop.Excel
+Imports System.Reflection
+
+Namespace KnowledgeBase.ComparingVSTOWithAspose
+    Public Class VSTOCode
+        Public Shared Sub Run()
+            Try
+                ' ExStart:1
+                ' The path to the documents directory.
+                Dim dataDir As String = RunExamples.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType)
+
+                Dim start As DateTime = DateTime.Now
+                Dim excelApp As Excel.Application = New Application()
+                Dim myPath As String = dataDir & Convert.ToString("TempBook.xls")
+                excelApp.Workbooks.Open(myPath, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, _
+                 Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, _
+                 Missing.Value, Missing.Value, Missing.Value)
+
+                For i As Integer = 1 To 1000
+                    For j As Integer = 1 To 20
+                        excelApp.Cells(i, j) = "Row " + i.ToString() + " " + "Col " + j.ToString()
+                    Next
+                Next
+
+                excelApp.Save(dataDir & Convert.ToString("TempBook1.xls"))
+                excelApp.Quit()
+                Dim [end] As DateTime = DateTime.Now
+                Dim time As TimeSpan = [end] - start
+                ' ExEnd:1
+                Console.WriteLine("File Created! " + "Time consumed (Seconds): " + time.TotalSeconds.ToString())
+            Catch ex As Exception
+                Console.WriteLine(ex.Message)
+            End Try
+        End Sub
+    End Class
+End Namespace
