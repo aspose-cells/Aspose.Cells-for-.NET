ea2bb48611468b6eb8d6a39d3b15648b55d48d9d
Shakeel Faiz <shakeel.faiz@aspose.com>
2017-07-02 10:11:38 +0500

    Revert "Revert "Revert "Export Range of Cells in a Worksheet to Image"""
    This reverts commit e3b0cb95173b927742232b7260d6ebce81b5138d.

--- Examples/CSharp/RunExamples.cs
+++ Examples/CSharp/RunExamples.cs
@@ -98,8 +98,6 @@ namespace Aspose.Cells.Examples.CSharp
             //Aspose.Cells for .NET v17.6
             DrawingObjects.TilePictureAsTextureInsideShape.Run();
             SmartMarkers.UsingFormulaParameterInSmartMarkerField.Run();
-            Rendering.ExportRangeOfCellsInWorksheetToImage.Run();
-            
 
             //Aspose.Cells for .NET v17.5
             //Data.ExportHTMLStringValueOfCellsToDataTable.Run();
