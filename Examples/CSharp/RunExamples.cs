84b8bbba91e7fe7178a0b9dc434d084d2e6fe10d
Shakeel Faiz <shakeel.faiz@aspose.com>
2018-01-15 15:09:53 +0500

    Aspose.Cells for .NET 17.12 - Run Examples

--- Examples/CSharp/RunExamples.cs
+++ Examples/CSharp/RunExamples.cs
@@ -112,13 +112,13 @@ namespace Aspose.Cells.Examples.CSharp
             // Uncomment the one you want to try out
 
             //Aspose.Cells for .NET v17.12
-            //Charts.SetShapeTypeOfDataLabelsOfChart.Run();
-            //HTML.ExportWorksheetCSSSeparatelyInOutputHTML.Run();
-            //HTML.PrefixTableElementsStylesWithHtmlSaveOptions_TableCssIdProperty.Run();
-            //Rendering.RenderOfficeAdd_InsWhileConvertingExcelToPdf.Run();
-            //SmartMarkers.AutoPopulateSmartMarkerDataToOtherWorksheets.Run();
-            //WorkbookSettings.Implement_Cell_FormulaLocal_SimilarTo_Range_FormulaLocal.Run();
-            //Worksheets.UpdateDaysPreservingHistoryOfRevisionLogsInSharedWorkbook.Run();
+            Charts.SetShapeTypeOfDataLabelsOfChart.Run();
+            HTML.ExportWorksheetCSSSeparatelyInOutputHTML.Run();
+            HTML.PrefixTableElementsStylesWithHtmlSaveOptions_TableCssIdProperty.Run();
+            Rendering.RenderOfficeAdd_InsWhileConvertingExcelToPdf.Run();
+            SmartMarkers.AutoPopulateSmartMarkerDataToOtherWorksheets.Run();
+            WorkbookSettings.Implement_Cell_FormulaLocal_SimilarTo_Range_FormulaLocal.Run();
+            Worksheets.UpdateDaysPreservingHistoryOfRevisionLogsInSharedWorkbook.Run();
 
             //Aspose.Cells for .NET v17.11
             //_Workbook.CreateSharedWorkbook.Run();
@@ -324,32 +324,24 @@ namespace Aspose.Cells.Examples.CSharp
             //ConvertWorksheettoImageFile.Run();
             //DeletingBlankColumns.Run();
             //DeletingBlankRows.Run();
-
             //ExtractOLEObjects.Run();
             //GenerateDatabarImage.Run();
             //SetPictureBackGroundFillChart.Run();
-
-
             //CopyingMultipleColumns.Run();
             //CopyingMultipleRows.Run();
             //CopyingSingleRow.Run();
             //CopyingSingleColumn.Run();
-
             //CopyChart.Run();
             //CopyControls.Run();
             //CopyingPicture.Run();
-
             //CreatePivotChart.Run();
             //CreatePivotTableWithFormatting.Run();
-
             //UseExplicitLineBreaks.Run();
             //WrapText.Run();
             //AbsolutePositionOfShapeInsideWorksheet.Run();
             //AddWordArtWatermarkToChart.Run();
             //ChangeShapesAdjustmentValues.Run();
-
-
-            CopyShapesBetweenWorksheets.Run();
+            //CopyShapesBetweenWorksheets.Run();
 
             //// CopyShapesBetweenWorksheets
             //// =====================================================
