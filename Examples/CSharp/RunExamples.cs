234428559bb549bc5af5b831c720241a62b85287
Shakeel Faiz <shakeel.faiz@aspose.com>
2018-01-27 10:29:36 +0500

    Aspose.Cells for .NET 17.12 - Run Examples

--- Examples/CSharp/RunExamples.cs
+++ Examples/CSharp/RunExamples.cs
@@ -112,13 +112,13 @@ namespace Aspose.Cells.Examples.CSharp
             // Uncomment the one you want to try out
 
             //Aspose.Cells for .NET v17.12
-            //Charts.SetShapeTypeOfDataLabelsOfChart.Run();
-            //HTML.ExportWorksheetCSSSeparatelyInOutputHTML.Run();
-            //HTML.PrefixTableElementsStylesWithHtmlSaveOptions_TableCssIdProperty.Run();
-            //Rendering.RenderOfficeAdd_InsWhileConvertingExcelToPdf.Run();
-            //SmartMarkers.AutoPopulateSmartMarkerDataToOtherWorksheets.Run();
-            //WorkbookSettings.Implement_Cell_FormulaLocal_SimilarTo_Range_FormulaLocal.Run();
-            //Worksheets.UpdateDaysPreservingHistoryOfRevisionLogsInSharedWorkbook.Run();
+            Charts.SetShapeTypeOfDataLabelsOfChart.Run();
+            HTML.ExportWorksheetCSSSeparatelyInOutputHTML.Run();
+            HTML.PrefixTableElementsStylesWithHtmlSaveOptions_TableCssIdProperty.Run();
+            Rendering.RenderOfficeAdd_InsWhileConvertingExcelToPdf.Run();
+            SmartMarkers.AutoPopulateSmartMarkerDataToOtherWorksheets.Run();
+            WorkbookSettings.Implement_Cell_FormulaLocal_SimilarTo_Range_FormulaLocal.Run();
+            Worksheets.UpdateDaysPreservingHistoryOfRevisionLogsInSharedWorkbook.Run();
 
             //Aspose.Cells for .NET v17.11
             //_Workbook.CreateSharedWorkbook.Run();
@@ -413,13 +413,12 @@ namespace Aspose.Cells.Examples.CSharp
             //SpecificPagesToImage.Run();
             //UseWorkbookRenderForImageConversion.Run();
             //WorksheetToAnImage.Run();
-
             //AddWorkbookScopedNamedRange.Run();
             //WorksheetNamedRange.Run();
             //CalculationOfArrayFormula.Run();
             //DecreaseCalculationTime.Run();
             //SetExternalLinksInFormulas.Run();
-            SettingSharedFormula.Run();
+            //SettingSharedFormula.Run();
 
 
             //// CopyShapesBetweenWorksheets
