54dbea221132d246b7ad067e501b3caee6f3246d
Shakeel Faiz <shakeel.faiz@aspose.com>
2018-02-08 15:38:03 +0500

    HandleAutomaticUnitsOfChartAxisLikeMicrosoftExcel

--- Examples/CSharp/Charts/HandleAutomaticUnitsOfChartAxisLikeMicrosoftExcel.cs
+++ Examples/CSharp/Charts/HandleAutomaticUnitsOfChartAxisLikeMicrosoftExcel.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Aspose.Cells;
+using Aspose.Cells.Charts;
+
 namespace Aspose.Cells.Examples.CSharp.Charts
 {
     class HandleAutomaticUnitsOfChartAxisLikeMicrosoftExcel
@@ -14,6 +17,18 @@ namespace Aspose.Cells.Examples.CSharp.Charts
 
             //Output directory
             string outputDir = RunExamples.Get_OutputDirectory();
+
+            //Load the sample Excel file
+            Workbook wb = new Workbook(sourceDir + "sampleHandleAutomaticUnitsOfChartAxisLikeMicrosoftExcel.xlsx");
+
+            //Access first worksheet
+            Worksheet ws = wb.Worksheets[0];
+
+            //Access first chart
+            Chart ch = ws.Charts[0];
+
+            //Render chart to pdf
+            ch.ToPdf(outputDir + "outputHandleAutomaticUnitsOfChartAxisLikeMicrosoftExcel.pdf");
 
             Console.WriteLine("HandleAutomaticUnitsOfChartAxisLikeMicrosoftExcel executed successfully.");
         }
