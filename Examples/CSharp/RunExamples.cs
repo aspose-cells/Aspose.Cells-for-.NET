d0e760e4676755f1a89f43f7ade400399468a78c
Shakeel Faiz <shakeel.faiz@aspose.com>
2018-03-16 12:46:56 +0500

    Aspose.Cells for .NET v18.2 - Run Examples

--- Examples/CSharp/RunExamples.cs
+++ Examples/CSharp/RunExamples.cs
@@ -106,11 +106,11 @@ namespace Aspose.Cells.Examples.CSharp
             // Uncomment the one you want to try out
 
             //Aspose.Cells for .NET v18.2
-            //WorkbookSettings.ControlExternalResourcesUsingWorkbookSetting_StreamProvider.Run();
-            //_Workbook.FilterDefinedNamesWhileLoadingWorkbook.Run();
-            //DrawingObjects.SetMarginsOfCommentOrShapeInsideTheWorksheet.Run();
-            //Data.SpecifyFormulaFieldsWhileImportingDataToWorksheet.Run();
-            //Formulas.SpecifyMaximumRowsOfSharedFormula.Run();
+            WorkbookSettings.ControlExternalResourcesUsingWorkbookSetting_StreamProvider.Run();
+            _Workbook.FilterDefinedNamesWhileLoadingWorkbook.Run();
+            DrawingObjects.SetMarginsOfCommentOrShapeInsideTheWorksheet.Run();
+            Data.SpecifyFormulaFieldsWhileImportingDataToWorksheet.Run();
+            Formulas.SpecifyMaximumRowsOfSharedFormula.Run();
 
             //Aspose.Cells for .NET v18.1
             //Charts.HandleAutomaticUnitsOfChartAxisLikeMicrosoftExcel.Run();
