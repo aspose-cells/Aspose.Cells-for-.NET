e42fcc969b2e304a915bd687f2fe2d5d4e8e4943
shakeel-faiz <shakeel.faiz@aspose.com>
2018-06-18 22:58:31 +0500

    SaveWorkbookToStrictOpenXMLSpreadsheetFormat

---
+++ Examples/CSharp/LoadingSavingConvertingAndManaging/SaveWorkbookToStrictOpenXMLSpreadsheetFormat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aspose.Cells.Examples.CSharp.LoadingSavingConvertingAndManaging
+{
+    class SaveWorkbookToStrictOpenXMLSpreadsheetFormat
+    {
+        //Source directory
+        static string sourceDir = RunExamples.Get_SourceDirectory();
+
+        //Output directory
+        static string outputDir = RunExamples.Get_OutputDirectory();
+
+        public static void Main()
+        {
+            // Create workbook.
+            Workbook wb = new Workbook();
+
+            // Specify - Strict Open XML Spreadsheet - Format.
+            wb.Settings.Compliance = OoxmlCompliance.Iso29500_2008_Strict;
+
+            // Add message in cell B4 of first worksheet.
+            Cell b4 = wb.Worksheets[0].Cells["B4"];
+            b4.PutValue("This Excel file has Strict Open XML Spreadsheet format.");
+
+            // Save to output Excel file.
+            wb.Save(outputDir + "outputSaveWorkbookToStrictOpenXMLSpreadsheetFormat.xlsx", SaveFormat.Xlsx);
+
+            Console.WriteLine("SaveWorkbookToStrictOpenXMLSpreadsheetFormat executed successfully.");
+        }
+    }
+}
--- Examples/CSharp/RunExamples.cs
+++ Examples/CSharp/RunExamples.cs
@@ -104,15 +104,18 @@ namespace Aspose.Cells.Examples.CSharp
 
 
             // Uncomment the one you want to try out
+            //Aspose.Cells for .NET v18.5
+            LoadingSavingConvertingAndManaging.SaveWorkbookToStrictOpenXMLSpreadsheetFormat.Main();
+
             //Aspose.Cells for .NET v18.4
-            Charts.CreateChartPDFWithDesiredPageSize.Main();
-            Charts.FindTypeOfXandYValuesOfPointsInChartSeries.Main();
-            Data.GetAllHiddenRowsIndicesAfterRefreshingAutoFilter.Main();
-            DocumentProperties.SpecifyLanguageOfExcelFileUsingBuiltInDocumentProperties.Main();
-            DrawingObjects.RotateTextWithShapeInsideWorksheet.Main();
-            HTML.GetHTML5StringFromCell.Main();
+            //Charts.CreateChartPDFWithDesiredPageSize.Main();
+            //Charts.FindTypeOfXandYValuesOfPointsInChartSeries.Main();
+            //Data.GetAllHiddenRowsIndicesAfterRefreshingAutoFilter.Main();
+            //DocumentProperties.SpecifyLanguageOfExcelFileUsingBuiltInDocumentProperties.Main();
+            //DrawingObjects.RotateTextWithShapeInsideWorksheet.Main();
+            //HTML.GetHTML5StringFromCell.Main();
             //HTML.HidingOverlaidContentWithCrossHideRightWhileSavingToHtml.Main();
-            Rendering.AvoidBlankPageInOutputPdfWhenThereIsNothingToPrint.Main();
+            //Rendering.AvoidBlankPageInOutputPdfWhenThereIsNothingToPrint.Main();
 
             //Aspose.Cells for .NET v18.3
             //Charts.ReadAxisLabelsAfterCalculatingTheChart.Run();
