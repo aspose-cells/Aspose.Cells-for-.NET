4707d308a0f4994036eab2fd82d88179e9518fdb
John He <john.he@aspose.com>
2021-11-01 16:06:45 +0800

    add CellsExplorer

---
+++ Examples/CellsExplorer/Model/CellsNodeType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CellsExplorer
+{
+    public enum CellsNodeType
+    {
+        RootNode,
+        LeafNode,
+        StructureNode
+    }
+}
