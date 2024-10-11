8a1870d0f1e15828228d1bdb947742671083f56f
peterzhou <peter.zhou@aspose.com>
2024-10-11 12:02:09 +0800

    update .net demo to v24.10

--- Examples_GridWeb/GridWeb.Net6/Controllers/GridController.cs
+++ Examples_GridWeb/GridWeb.Net6/Controllers/GridController.cs
@@ -11,6 +11,12 @@ namespace GridWeb.Demo.Controllers
     [Route("grid")]
     public class GridController : Controller
     {
+        public class SimpleViewModel
+        {
+            public string EnteredValue { get; set; }
+        }
+
+
         [HttpGet("versionp/{type}/{id}")]
         public IActionResult GetV1(string type, string id)
         {
@@ -30,9 +36,37 @@ namespace GridWeb.Demo.Controllers
             //the default auto predefined post action 
             return Aspose.Cells.GridWeb.AcwController.DoAcwAction(this, type, id);
         }
+        
+        [HttpPost]  
+        public IActionResult ButtonClickUpdateA1(SimpleViewModel model)
+        {
+            Aspose.Cells.GridWeb.GridWeb mw = new Aspose.Cells.GridWeb.GridWeb();
+           
+            //use this method to restore GridWeb instance
+            mw.RestoreBySession(HttpContext.Session);
+            string a1v = mw.ActiveSheet.Cells["A1"].StringValue;
+            mw.ActiveSheet.Cells["A1"].PutValue(model.EnteredValue);
+            mw.ActiveSheet.Cells["A1"].CreateComment("hello world" + " " + new DateTime(), "peter", true);
+            //call update cache to save back the edit result
+            mw.UpdateCache();
+            //return   Content(mw.ActiveSheet.Cells["A1"].StringValue); ;  
+           return  RedirectToAction("Index1", new { value = "fromupdate" });
+        }
+
+        [HttpGet] //  get  value from cell c3
+        public IActionResult QueryInfo()
+        {
+            Aspose.Cells.GridWeb.GridWeb mw = new Aspose.Cells.GridWeb.GridWeb();
+
+            //use this method to restore GridWeb instance
+            mw.RestoreBySession(HttpContext.Session);
+            string a1v = mw.ActiveSheet.Cells["C3"].StringValue;
+           
+            return Content(a1v);
+        }
 
         [HttpGet("index1")]
-        public IActionResult Index1()
+        public IActionResult Index1(string value)
         {
             //set a session store path
             Aspose.Cells.GridWeb.GridWeb.SessionStorePath = TestConfig.TempDir;
@@ -44,10 +78,18 @@ namespace GridWeb.Demo.Controllers
 			 //set the picture cache ,you need to create this directory
             mw.PictureCachePath = TestConfig.PicDir;
             mw.EnableAsync = true;
+            mw.ShowCellEditBox = true;
             //load workbook
             //String file = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\wb\test.xlsx");
             String file = TestConfig.File;
-            mw.ImportExcelFile(file);
+            if (value == null)
+            {
+                mw.ImportExcelFile(file);
+            }
+            else
+            {//fromupdate
+                mw.RestoreBySession(HttpContext.Session);
+            }
             mw.ActiveSheet.Cells["B1"].PutValue("version:");
             mw.ActiveSheet.Cells["C1"].PutValue(Aspose.Cells.GridWeb.GridWeb.GetVersion());
 
